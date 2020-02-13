fa70bc8922f6a9202cea074c5b646fcf00b05c5a
Pierre Aubert <pierreaubert@yahoo.fr>
2020-02-13 21:16:04 +0100

    add metadata for a better presentation

---
+++ datas/metadata.py
@@ -0,0 +1,60 @@
+speakes_metadata = {
+    'Dayton Audio B652-AIR'  = {
+        'type': '',
+        'price': '',
+        },
+    'JBL 104' = {
+        'type': '',
+        'price': '',
+        },
+    'Kali IN-8': {
+        'type': 'active',
+        'price': '',
+        },
+    'Pioneer SP-BS22-LR' = {
+        'type': '',
+        'price': '',
+        },
+    'Revel C52' = {
+        'type': '',
+        'price': '',
+        },
+    'Emotiva Airmotive 6s' = {
+        'type': '',
+        'price': '',
+        },
+    'JBL Control 1 Pro' = {
+        'type': '',
+        'price': '',
+        },
+    'Micca RB42' = {
+        'type': '',
+        'price': '',
+        },
+    'Selah Audio RC3R' = {
+        'type': '',
+        'price': '',
+        },
+    'Harbeth Monitor' = {
+        'type': '',
+        'price': '',
+        },
+    'KEF LS50' = {
+        'type': '',
+        'price': '',
+        },
+    'Neumann KH 80' = {
+        'type': '',
+        'price': '',
+        },
+    'Realistic MC-1000' = {
+        'type': '',
+        'price': '',
+        },
+     'Yamaha HS5'  = {
+        'type': '',
+        'price': '',
+        },
+}
+
+
