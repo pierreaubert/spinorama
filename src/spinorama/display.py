06e300ca1eefbabea01ab49ce3c9c805f84ee24c
Pierre Aubert <pierreaubert@yahoo.fr>
2020-02-15 10:16:59 +0100

    add datas for Adam S2V

--- src/spinorama/display.py
+++ src/spinorama/display.py
@@ -19,7 +19,7 @@ def graph_freq_sidebyside(s1, s2, name, width=450, heigth=450):
 def display_contour_horizontal(df, speaker, width=400, heigth=180):
     try:
         dfs = df[speaker]['SPL Horizontal_unmelted']
-        return graph_contour_smoothed(dfs, width, heigth)
+        return graph_contour(dfs, width, heigth)
     except KeyError:
         return None
 
@@ -27,7 +27,7 @@ def display_contour_horizontal(df, speaker, width=400, heigth=180):
 def display_contour_vertical(df, speaker, width=400, heigth=180):
     try:
         dfs = df[speaker]['SPL Vertical_unmelted']
-        return graph_contour_smoothed(dfs, width, heigth)
+        return graph_contour(dfs, width, heigth)
     except KeyError:
         return None
 
@@ -72,8 +72,8 @@ def display_contour_sidebyside(df, speaker, width=450, heigth=450):
         contourH = df[speaker]['SPL Horizontal_unmelted']
         contourV = df[speaker]['SPL Vertical_unmelted']
         return alt.hconcat(
-            graph_contour_smoothed(contourH, width, heigth),
-            graph_contour_smoothed(contourV, width, heigth))
+            graph_contour(contourH, width, heigth),
+            graph_contour(contourV, width, heigth))
     except KeyError:
         return None
 
@@ -172,55 +172,3 @@ def display_spl_vertical(df, speaker, width, heigth):
     return display_spl(df, speaker, 'SPL Vertical', width, heigth)
 
 
-def display_template1(df, speaker, width=900, heigth=500):
-    width2 = math.floor(width * 45 / 100)
-    heigth2 = math.floor(heigth * 45 / 100)
-    width3 = math.floor(width * 30 / 100)
-    heigth3 = math.floor(heigth * 30 / 100)
-    # full size
-    spinorama = display_spinorama(df, speaker, width, heigth)
-    # side by side
-    onaxis = display_onaxis(df, speaker, width2, heigth2)
-    inroom = display_inroom(df, speaker, width2, heigth2)
-    # side by side
-    ereflex = display_reflection_early(df, speaker, width3, heigth3)
-    hreflex = display_reflection_horizontal(df, speaker, width3, heigth3)
-    vreflex = display_reflection_vertical(df, speaker, width3, heigth3)
-    # side by side
-    hspl = display_spl_horizontal(df, speaker, width2, heigth2)
-    vspl = display_spl_vertical(df, speaker, width2, heigth2)
-    # side by side
-    hcontour = display_contour_horizontal(df, speaker, width2, heigth2)
-    vcontour = display_contour_vertical(df, speaker, width2, heigth2)
-    return alt.vconcat(spinorama,
-                       onaxis | inroom,
-                       ereflex | hreflex | vreflex,
-                       hspl | vspl,
-                       hcontour | vcontour)
-
-
-def display_vertical(df, speaker, width=900, heigth=500):
-    spinorama = display_spinorama(df, speaker, width, heigth)
-    onaxis = display_onaxis(df, speaker, width, heigth)
-    inroom = display_inroom(df, speaker, width, heigth)
-    ereflex = display_reflection_early(df, speaker, width, heigth)
-    hreflex = display_reflection_horizontal(df, speaker, width, heigth)
-    vreflex = display_reflection_vertical(df, speaker, width, heigth)
-    hspl = display_spl_horizontal(df, speaker, width, heigth)
-    vspl = display_spl_vertical(df, speaker, width, heigth)
-    hcontour = display_contour_horizontal(df, speaker, width, heigth)
-    hradar = display_radar_horizontal(df, speaker, width, heigth)
-    vcontour = display_contour_vertical(df, speaker, width, heigth)
-    vradar = display_radar_vertical(df, speaker, width, heigth)
-    return alt.vconcat(spinorama,
-                       onaxis,
-                       inroom,
-                       ereflex,
-                       hreflex,
-                       vreflex,
-                       hspl,
-                       vspl,
-                       hcontour,
-                       hradar,
-                       vcontour,
-                       vradar)
