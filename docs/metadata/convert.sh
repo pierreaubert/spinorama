1a92bb156ee1f37117c8209a0b42de6d5c24c7ef
Pierre Aubert <pierreaubert@yahoo.fr>
2020-02-15 10:22:41 +0100

    add datas for Adam S2V

--- docs/metadata/convert.sh
+++ docs/metadata/convert.sh
@@ -1,1 +1,7 @@
-for i in originals/*.jpg; do convert "$i" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "${i#originals/}"; done
+for i in originals/*.jpg; do 
+   smaller="${i#originals/}"
+   if ! test -f "$smaller"; then
+       echo convert "$i" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "$smaller";
+       convert "$i" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "$smaller";
+   fi
+done
