4e26d8740bb4142dae972e382494442e3c4b880e
Pierre Aubert <pierreaubert@yahoo.fr>
2020-02-17 21:53:27 +0100

    load json file generated by webplotanalyser

--- src/spinorama/analysis.py
+++ src/spinorama/analysis.py
@@ -13,23 +13,27 @@ def fconst(x, a):
 
 
 def estimates(onaxis: pd.DataFrame):
-    xdata1 = np.array(onaxis.loc[onaxis['Freq'] < 60].Freq)
-    ydata1 = np.array(onaxis.loc[onaxis['Freq'] < 60].dB)
+    try:
+        xdata1 = np.array(onaxis.loc[onaxis['Freq'] < 60].Freq)
+        ydata1 = np.array(onaxis.loc[onaxis['Freq'] < 60].dB)
 
-    popt1, pcov1 = curve_fit(flinear, xdata1, ydata1)
+        popt1, pcov1 = curve_fit(flinear, xdata1, ydata1)
 
-    xdata2 = np.array(onaxis.loc[onaxis['Freq'] >= 100].Freq)
-    ydata2 = np.array(onaxis.loc[onaxis['Freq'] >= 100].dB)
+        xdata2 = np.array(onaxis.loc[onaxis['Freq'] >= 100].Freq)
+        ydata2 = np.array(onaxis.loc[onaxis['Freq'] >= 100].dB)
 
-    popt2, pcov2 = curve_fit(fconst, xdata2, ydata2)
+        popt2, pcov2 = curve_fit(fconst, xdata2, ydata2)
 
-    inter = math.exp((popt2[0] - popt1[1]) / popt1[0])
-    inter_3 = math.exp((popt2[0] - popt1[1] - 3) / popt1[0])
-    inter_6 = math.exp((popt2[0] - popt1[1] - 6) / popt1[0])
+        inter = math.exp((popt2[0] - popt1[1]) / popt1[0])
+        inter_3 = math.exp((popt2[0] - popt1[1] - 3) / popt1[0])
+        inter_6 = math.exp((popt2[0] - popt1[1] - 6) / popt1[0])
 
-    # search band up/down
-    up: float = ydata2.max() - popt2[0]
-    down: float = ydata2.min() - popt2[0]
+        # search band up/down
+        up: float = ydata2.max() - popt2[0]
+        down: float = ydata2.min() - popt2[0]
 
-    return [int(inter), int(inter_3), int(inter_6),
-            math.floor(max(up, -down) * 10) / 10]
+        return [int(inter), int(inter_3), int(inter_6),
+                math.floor(max(up, -down) * 10) / 10]
+    except TypeError:
+        print('Type error:')
+        return [-1,-1,-1,-1]
