16af0a5b57c204b486b11d0f9676bf35c1369b4b
Pierre Aubert <pierreaubert@yahoo.fr>
2020-02-22 13:03:29 +0100

    code support multiple measurements and multiple source of measurements

--- src/spinorama/print.py
+++ src/spinorama/print.py
@@ -15,44 +15,44 @@ def print_graph(speaker, origin, key, title, chart, width, height, force, fileex
         pathlib.Path(filedir).mkdir(parents=True, exist_ok=True)
         for ext in ['json', 'png', 'html']:  # svg skipped slow
             filename = filedir + '/' + title.replace('_unmelted', '') + '.' + ext
+            # print(filename)
             if force or not os.path.exists(filename):
                 if fileext is None or (fileext is not None and fileext == ext):
                     chart.save(filename)
                     updated += 1
+    else:
+        print('Chart is None for {:s} {:s} {:s} {:s}'.format(speaker, origin, key, title))
     return updated
 
 
-def print_graphs(df, speaker, origin, key, width=900, height=500, force_print=False, filter_file_ext=None):
+def print_graphs(df,
+                 speaker, origin, key='default',
+                 width=900, height=500,
+                 force_print=False, filter_file_ext=None):
     # may happens at development time
     if df is None:
+        print('Error: print_graph is None')
         return 0
 
     graphs = {}
-    graphs['CEA2034'] = display_spinorama(df, speaker, width, height)
-    graphs['On Axis'] = display_onaxis(df, speaker, width, height)
-    graphs['Estimated In-Room Response'] = display_inroom(
-        df, speaker, width, height)
-    graphs['Early Reflections'] = display_reflection_early(
-        df, speaker, width, height)
-    graphs['Horizontal Reflections'] = display_reflection_horizontal(
-        df, speaker, width, height)
-    graphs['Vertical Reflections'] = display_reflection_vertical(
-        df, speaker, width, height)
-    graphs['SPL Horizontal'] = display_spl_horizontal(
-        df, speaker, width, height)
-    graphs['SPL Vertical'] = display_spl_vertical(df, speaker, width, height)
-    graphs['SPL Horizontal Contour'] = display_contour_horizontal(
-        df, speaker, width, height)
-    graphs['SPL Vertical Contour'] = display_contour_vertical(
-        df, speaker, width, height)
+    graphs['CEA2034'] = display_spinorama(df, width, height)
+    graphs['On Axis'] = display_onaxis(df, width, height)
+    graphs['Estimated In-Room Response'] = display_inroom(df, width, height)
+    graphs['Early Reflections'] = display_reflection_early(df, width, height)
+    graphs['Horizontal Reflections'] = display_reflection_horizontal(df, width, height)
+    graphs['Vertical Reflections'] = display_reflection_vertical(df, width, height)
+    graphs['SPL Horizontal'] = display_spl_horizontal(df, width, height)
+    graphs['SPL Vertical'] = display_spl_vertical(df, width, height)
+    graphs['SPL Horizontal Contour'] = display_contour_horizontal(df, width, height)
+    graphs['SPL Vertical Contour'] = display_contour_vertical(df, width, height)
     # 1080p to 2k screen
-    graphs['2cols'] = template_compact(df, speaker, width, height)
+    graphs['2cols'] = template_compact(df, width, height)
     # 4k screen
-    graphs['3cols'] = template_panorama(df, speaker, width*3, height*3)
+    graphs['3cols'] = template_panorama(df, width*3, height*3)
     # better square
     size = max(width, height)
-    graphs['SPL Horizontal Radar'] = display_radar_horizontal(df, speaker, size, size)
-    graphs['SPL Vertical Radar'] = display_radar_vertical(df, speaker, size, size)
+    graphs['SPL Horizontal Radar'] = display_radar_horizontal(df, size, size)
+    graphs['SPL Vertical Radar'] = display_radar_vertical(df, size, size)
 
     updated = 0
     for (title, graph) in graphs.items():
