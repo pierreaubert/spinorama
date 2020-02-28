26e4813008d58617a929539ef830a70aec92b146
Pierre Aubert <pierreaubert@yahoo.fr>
2020-02-28 23:30:37 +0100

    start to add tests, cov is close to 0

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -2,7 +2,7 @@
 import unittest
 # import logging
 from spinorama.load import parse_graph_freq_klippel, graph_melt
-from spinorama.analysis import estimates
+from spinorama.analysis import estimates, cea2034
 
 
 class SpinoramaAnalysisTests(unittest.TestCase):
@@ -23,6 +23,26 @@ class SpinoramaAnalysisTests(unittest.TestCase):
         self.assertAlmostEqual(self.estimates[2], 54)
         self.assertAlmostEqual(self.estimates[3], 3.0)
 
+
+class SpinoramaSpinoramaTests(unittest.TestCase):
+
+    def setUp(self):
+        # load spin
+        self.title, self.df_unmelted = parse_graph_freq_klippel('datas/ASR/Neumann KH 80/CEA2034.txt')
+        self.df = graph_melt(self.df_unmelted)
+        self.onaxis = self.df.loc[self.df['Measurements'] == 'On Axis']
+        # load spl vertical and horizontal
+        self.titleH, self.splH = parse_graph_freq_klippel('datas/ASR/Neumann KH 80/SPL Horizontal.txt')
+        self.titleV, self.splV = parse_graph_freq_klippel('datas/ASR/Neumann KH 80/SPL Vertical.txt')
+        
+    def test_validate_cea2034(self):
+        computed_spin_unmelted = cea2034(self.splH, self.splV)
+        computed_spin = graph_melt(computed_spin_unmelted)
+        computed_onaxis = computed_spin.loc[computed_spin['Measurements'] == 'On Axis']
+        # self.assertEqual(computed_onaxi.Freq.size , self.onaxis.Freq.size)
+        pass
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
