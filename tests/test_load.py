afb515f1e84462a6527b9e319fd1ff2efdbd5589
Pierre Aubert <pierreaubert@yahoo.fr>
2020-02-28 23:30:37 +0100

    start to add tests, cov is close to 0

--- tests/test_load.py
+++ tests/test_load.py
@@ -14,7 +14,24 @@ class SpinoramaLoadTests(unittest.TestCase):
         self.assertIsNotNone(self.df)
 
     def test_smoke2(self):
-        self.assertIn('On Axis', self.df.keys())
+        self.assertIn('On Axis', self.df.columns)
+        self.assertNotIn('On-Axis', self.df.columns)
+
+
+class SpinoramaLoadSPLTests(unittest.TestCase):
+
+    def setUp(self):
+        self.title, self.df = parse_graph_freq_klippel('datas/ASR/Neumann KH 80/SPL Horizontal.txt')
+
+    def test_smoke1(self):
+        self.assertEqual(self.title, 'SPL Horizontal')
+        self.assertIsNotNone(self.df)
+
+    def test_smoke2(self):
+        self.assertIn('On Axis', self.df.columns)
+        self.assertNotIn('On-Axis', self.df.columns)
+        # 200 in Freq, 36 off axis and 0
+        self.assertEqual(self.df.shape, (200, 37))
 
 
 if __name__ == '__main__':
