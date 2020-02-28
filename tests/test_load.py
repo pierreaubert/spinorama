21a2335cb7bdd1773bdfec5fca1fe83cfee2035f
Pierre Aubert <pierreaubert@yahoo.fr>
2020-02-28 22:42:59 +0100

    start to add tests, cov is close to 0

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,13 +1,20 @@
 # import os
 import unittest
 # import logging
-# from spinorama import load
+from spinorama.load import parse_graph_freq_klippel
 
 
 class SpinoramaLoadTests(unittest.TestCase):
 
-    def test_smoke(self):
-        pass
+    def setUp(self):
+        self.title, self.df = parse_graph_freq_klippel('datas/ASR/Neumann KH 80/CEA2034.txt')
+
+    def test_smoke1(self):
+        self.assertEqual(self.title, 'CEA2034')
+        self.assertIsNotNone(self.df)
+
+    def test_smoke2(self):
+        self.assertIn('On Axis', self.df.keys())
 
 
 if __name__ == '__main__':
