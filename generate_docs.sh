7156140254926a3a32d71f7578942224d164f26a
Pierre Aubert <pierreaubert@yahoo.fr>
2020-03-08 07:39:11 +0100

    add Tannoy 600 data

--- generate_docs.sh
+++ generate_docs.sh
@@ -2,7 +2,7 @@ echo "Starting Generation"
 export PYTHONPATH=src:.
 rm -fr docs
 mkdir docs docs/assets docs/pictures
-python3 ./generate_graph.py
+python3 ./generate_graphs.py
 ./minimise_pictures.sh
 python3 ./generate_meta.py
 python3 ./generate_html.py
