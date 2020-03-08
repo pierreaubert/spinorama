04e86083f2a24d323f29acb543e696b61636f4fd
Pierre Aubert <pierreaubert@yahoo.fr>
2020-03-08 09:34:49 +0100

    decrease size of docs/

--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,10 +1,8 @@
-window.$ = window.jQuery;
+window.$ = window.jQuery
 
 $(document).ready(function () {
-
     window.$.getJSON('https://pierreaubert.github.io/spinorama/assets/metadata.json', function (response) {
-
-	const metadata = Object.values(response);
+	const metadata = Object.values(response)
 	const fuse = new Fuse(metadata, {
 	    matchAllTokens: true,
 	    findAllMatches: true,
@@ -12,43 +10,44 @@ $(document).ready(function () {
 	    keys: ['brand', 'model', 'type', 'measurements.origin'],
 	    treshhold: 0.0,
 	    distance: 1,
-	    includeScore: true,
-	});
+	    includeScore: true
+	})
 	
 	$('#searchInput').on('keyup', function () {
-	    let resultdiv = $('div.searchresults');
-	    let keywords = $(this).val();
+	    const resultdiv = $('div.searchresults')
+	    const keywords = $(this).val()
 	    if (keywords.length === 0) {
-		for( let item in response ) {
-		    resultdiv.hide();
-		}	
-		resultdiv.show();
-	    } else { 
-		let result = fuse.search(keywords);
-		if (result.length === 0) {
-		    resultdiv.hide();
-		} else {
-		    for (let item in metadata) {
-			let id = (metadata[item].brand + '-' + metadata[item].model).replace(/['. ]/g, '-');
-			// console.log('hide:'+id);
-			$('#'+id).hide();
-		    }
-		    let minScore = 1;
-		    for (let item in result) {
-			if( result[item].score < minScore ) {
-			    minScore = result[item].score;
-			}
-		    }
-		    for (let item in result) {
-			let id = (result[item].item.brand + '-' + result[item].item.model).replace(/['. ]/g, '-');
-			if( (minScore > 0.0) || (result[item].score === 0.0 )) {
-			    // console.log('show:'+id+' maxscore:' + minScore+' score:' + result[item].score);
-			    $('#'+id).show();
-			}
-		    }
-		    resultdiv.show();
+		for (const item in metadata) {
+		    const id = (metadata[item].brand + '-' + metadata[item].model).replace(/['. ]/g, '-')
+		    $('#' + id).show()
 		}
-	    }	 
-	});
-    });
-});
+		resultdiv.show()
+	    } else {
+		const result = fuse.search(keywords)
+		if (result.length === 0) {
+		    resultdiv.hide()
+		} else {
+		    for (const item in metadata) {
+			const id = (metadata[item].brand + '-' + metadata[item].model).replace(/['. ]/g, '-')
+			// console.log('hide:'+id);
+			$('#' + id).hide()
+		    }
+		    let minScore = 1
+		    for (const item in result) {
+			if (result[item].score < minScore) {
+			    minScore = result[item].score
+			}
+		    }
+		    for (const item in result) {
+			const id = (result[item].item.brand + '-' + result[item].item.model).replace(/['. ]/g, '-')
+			if ((minScore > 0.0) || (result[item].score === 0.0)) {
+			    // console.log('show:'+id+' maxscore:' + minScore+' score:' + result[item].score);
+			    $('#' + id).show()
+			}
+		    }
+		    resultdiv.show()
+		}
+	    }
+	})
+    })
+})
