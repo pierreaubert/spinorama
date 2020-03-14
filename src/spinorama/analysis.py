92f58dd30e6724f8f4a0223d5db60d2f325570df
Pierre Aubert <pierreaubert@yahoo.fr>
2020-03-14 07:45:59 +0100

    replace inroom by pir in pref scores

--- src/spinorama/analysis.py
+++ src/spinorama/analysis.py
@@ -305,7 +305,6 @@ def sound_power(h_spl: pd.DataFrame, v_spl: pd.DataFrame) -> pd.DataFrame:
         return False
 
     def rms(spl):
-        print(len([c for c in sp_cols if column_valid(c)]))
         avg = [(sp_weigths[column_trim(c)] * spl2pressure(spl[c]))**2 for c in sp_cols if column_valid(c)]
         wsm = [sp_weigths[column_trim(c)]**2 for c in sp_cols if column_valid(c)]
         return pressure2spl(np.sqrt(np.sum(avg)/np.sum(wsm)))
@@ -383,7 +382,10 @@ def compute_cea2034(h_spl: pd.DataFrame, v_spl: pd.DataFrame) -> pd.DataFrame:
 
 # https://courses.physics.illinois.edu/phys406/sp2017/Lab_Handouts/Octave_Bands.pdf
 def octave(N):
-    """compute 1/N octave band"""
+    """compute 1/N octave band
+
+    N: >=2 when N increases, bands are narrower
+    """
     p = pow(2,1/N)
     p_band= pow(2,1/(2*N))
     iter = int((N*10+1)/2)
@@ -392,6 +394,9 @@ def octave(N):
 
 
 def aad(dfu):
+    """ aad Absolute Average Deviation
+    """
+    
     # mean betwenn 200hz and 400hz
     y_ref = np.mean(dfu.loc[(dfu.Freq>=200) & (dfu.Freq<=400)].dB)
     # print(y_ref)
@@ -414,7 +419,18 @@ def aad(dfu):
     #    print(aad_sum, n, dfu)
     return aad_value
 
+
 def nbd(dfu):
+    """ nbd Narrow Band
+
+    The narrow band deviation is defined by:
+      NBD(dB)=⎜ ∑ y −y ⎟÷N  ⎛
+    where ⎜ OctaveBandn⎟ is the average amplitude value
+    within the 1⁄2-octave band n, yb is the amplitude
+    value of band b, and N is the total number of 1⁄2­ octave bands 
+    between 100 Hz-12 kHz. The mean absolute deviation within each 
+    1⁄2-octave band is based a sample of 10 equally log-spaced data points.
+    """
     sum = 0
     n = 0
     # 1/2 octave
@@ -436,6 +452,18 @@ def nbd(dfu):
 
 
 def lfx(lw, sp):
+    """ lfx Low Frequency Extension
+
+    The low frequency extension (LFX) 
+         LFX = log10(xSP−6dB.re:y _ LW(300Hz−10kHz) (7)
+    where LFX is the log10 of the first frequency x_SP below 300 Hz
+    in the sound power curve, that is -6 dB relative to the mean level y_LW 
+    measured in listening window (LW) between 300 Hz-10 kHz. 
+    LFX is log-transformed to produce a linear relationship between the 
+    variable LFX and preference rating. The sound power curve (SP) is used 
+    for the calculation because it better defines the true bass output of
+    the loudspeaker, particularly speakers that have rear-firing ports.
+    """
     y_ref = np.mean(lw.loc[(lw.Freq>=300) & (lw.Freq<=10000)].dB)-6
     # find first freq such that y[freq]<y_ref-6dB
     y = math.log10(sp.loc[(sp.Freq<300)&(sp.dB<=y_ref)].Freq.max())
@@ -443,6 +471,11 @@ def lfx(lw, sp):
 
 
 def lfq(lw, sp, lfx_log):
+    """ lfq Low Frequency Quality
+
+    LFQ is intended to quantify deviations in amplitude response over the
+    bass region between the low frequency cut-off and 300 Hz.
+    """
     lfx = pow(10,lfx_log)
     sum = 0
     n = 0
@@ -462,6 +495,23 @@ def lfq(lw, sp, lfx_log):
     return sum/n
 
 def sm(dfu):
+    """ sm Smoothness
+
+    For each of the 7 frequency response curves, the overall smoothness (SM) and
+    slope (SL) of the curve was determined by estimating the line that best fits
+    the frequency curve over the range of 100 Hz-16 kHz. This was done using a
+    regression based on least square error. SM is the Pearson correlation 
+    coefficient of determination (r2) that describes the goodness of fit of the
+    regression line defined by:
+    ⎛ SM =⎜ n(∑XY)−(∑X)(∑Y) ⎟ / ⎜ (n∑X2 −(∑X)2)(n∑Y2 −(∑Y)2)⎟
+
+    where n is number of data points used to estimate the regression curve and 
+    X and Y represent the measured versus estimated amplitude values of the 
+    regression line. A natural log transformation is applied to the measured 
+    frequency values (Hz) so that they are linearly spaced (see equation 5). 
+    Smoothness (SM) values can range from 0 to 1, with larger values representing 
+    smoother frequency response curves.
+    """
     data = dfu.loc[(dfu.Freq>=100) & (dfu.Freq<=16000)]
     slope, intercept, r_value, p_value, std_err = linregress(data.Freq, data.dB)
     return r_value**2
@@ -471,7 +521,7 @@ def pref_rating(nbd_on, nbd_pir, lfx, sm_pir):
     return 12.69-2.49*nbd_on-2.99*nbd_pir-4.31*lfx+2.32*sm_pir
 
 
-def speaker_pref_rating(cea2034):
+def speaker_pref_rating(cea2034, df_pred_in_room):
     df_on_axis = cea2034.loc[lambda df: df.Measurements == 'On Axis']
     df_listening_window = cea2034.loc[lambda df: df.Measurements == 'Listening Window']
     df_sound_power = cea2034.loc[lambda df: df.Measurements == 'Sound Power']
@@ -483,17 +533,21 @@ def speaker_pref_rating(cea2034):
     nbd_on_axis = nbd(df_on_axis)
     nbd_listening_window = nbd(df_listening_window)
     nbd_sound_power = nbd(df_sound_power)
+    nbd_pred_in_room = nbd(df_pred_in_room)
     lfx_hz = lfx(df_listening_window, df_sound_power)
     lfq_db = lfq(df_listening_window, df_sound_power, lfx_hz)
     sm_sound_power = sm(df_sound_power)
-    pref = pref_rating(nbd_on_axis, nbd_sound_power, lfx_hz, sm_sound_power)
+    sm_pred_in_room = sm(df_pred_in_room)
+    pref = pref_rating(nbd_on_axis, nbd_pred_in_room, lfx_hz, sm_pred_in_room)
     ratings = {
         'aad_on_axis': round(aad_on_axis, 2),
         'nbd_on_axis': round(nbd_on_axis, 2),
         'nbd_listening_window': round(nbd_listening_window, 2),
         'nbd_sound_power': round(nbd_sound_power, 2),
+        'nbd_pred_in_room': round(nbd_pred_in_room, 2),
         'lfx_hz': int(pow(10, lfx_hz)), # in Hz
         'lfq': round(lfq_db, 2),
+        'sm_pred_in_room': round(sm_pred_in_room, 2),
         'sm_sound_power': round(sm_sound_power, 2),
         'pref_score': round(pref, 1),
     }
