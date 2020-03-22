701277343effc4d4e4ac9cdf2e7c06a9686a5212
Pierre Aubert <pierreaubert@yahoo.fr>
2020-03-22 21:53:32 +0100

    add --delete to rsync

--- update_website.sh
+++ update_website.sh
@@ -3,7 +3,7 @@ TARGET=$HOME/src/pierreaubert.github.io/spinorama
 ./minimise_pictures.sh
 ./generate_stats.py
 ./generate_html.py
-rsync -arv docs/* $TARGET
+rsync -arv --delete docs/* $TARGET
 rm  $TARGET/[A-Z]*/*/*/*.png
 cd $TARGET && git status
 
