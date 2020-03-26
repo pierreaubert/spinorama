67e39930bf57c6d0a804b2d575a09f805c97fee3
Pierre Aubert <pierreaubert@yahoo.fr>
2020-03-26 12:28:09 +0100

    add missing commands

--- update_website.sh
+++ update_website.sh
@@ -1,6 +1,8 @@
 TARGET=$HOME/src/pierreaubert.github.io/spinorama
 
 ./minimise_pictures.sh
+./generate_graphs.py --only-compare=True
+./generate_meta.py
 ./generate_stats.py
 ./generate_html.py
 rsync -arv --delete docs/* $TARGET
