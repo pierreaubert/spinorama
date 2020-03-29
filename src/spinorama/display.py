45f20c4475d9c39f848a277e755a3b0c677628b5
Pierre Aubert <pierreaubert@yahoo.fr>
2020-03-29 14:42:38 +0200

    add regression lines for On Axis and Estimated In-Room Response

--- src/spinorama/display.py
+++ src/spinorama/display.py
@@ -5,7 +5,8 @@ from .normalize import resample
 from .graph import graph_freq, graph_contour_smoothed, graph_radar, graph_spinorama,\
     graph_params_default, contour_params_default, radar_params_default, \
     graph_contour, graph_directivity_matrix,\
-    graph_compare_freq, graph_compare_cea2034
+    graph_compare_freq, graph_compare_cea2034, graph_compare_freq_regression, \
+    graph_regression
 
 
 alt.data_transformers.disable_max_rows()
@@ -125,15 +126,7 @@ def display_onaxis(df, graph_params=graph_params_default):
         onaxis = df['CEA2034']
         onaxis = onaxis.loc[onaxis['Measurements'] == 'On Axis']
         onaxis_graph = graph_freq(onaxis, graph_params)
-        onaxis_reg = alt.Chart(onaxis).transform_filter(
-            'datum.Freq>80 & datum.Freq<18000'
-        ).transform_regression(
-            "Freq", "dB"
-        ).mark_line().encode(
-            alt.X('Freq:Q'),
-            alt.Y('dB:Q'),
-            color=alt.value('red')
-        )
+        onaxis_reg = graph_regression(onaxis, 80, 10000)
         return onaxis_graph + onaxis_reg
     except KeyError as ke:
         logging.warning('Display On Axis failed with {0}'.format(ke))
@@ -149,15 +142,7 @@ def display_inroom(df, graph_params=graph_params_default):
             return None
         inroom = df['Estimated In-Room Response']
         inroom_graph = graph_freq(inroom, graph_params)
-        inroom_reg = alt.Chart(inroom).transform_filter(
-            'datum.Freq>100 & datum.Freq<10000'
-        ).transform_regression(
-            "Freq", "dB"
-        ).mark_line().encode(
-            alt.X('Freq:Q'),
-            alt.Y('dB:Q'),
-            color=alt.value('red')
-        )
+        inroom_reg = graph_regression(inroom, 80, 10000)
         return inroom_graph + inroom_reg
     except KeyError as ke:
         logging.warning('Display In Room failed with {0}'.format(ke))
@@ -245,6 +230,8 @@ def display_compare(df, graph_filter, graph_params=graph_params_default):
 
         if graph_filter == 'CEA2034':
             graph = graph_compare_cea2034(source, graph_params, speaker1, speaker2)
+        elif graph_filter in ('On Axis', 'Estimated In-Room Response'):
+            graph = graph_compare_freq_regression(source, graph_params, speaker1, speaker2)
         else:
             graph = graph_compare_freq(source, graph_params, speaker1, speaker2)
         return graph
