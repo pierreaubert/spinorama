61cacd5e23d8582c37f58be54e2916bc7a6c391b
Pierre Aubert <pierreaubert@yahoo.fr>
2020-04-13 13:56:59 +0200

    add Revel Ultima Salon

---
+++ datas/Vendors/Revel/Revel Ultima Salon/README.mk
@@ -0,0 +1,3 @@
+Revel Salon from 1967
+Measurement from Harmann 2019
+Scan from https://speakerdata2034.blogspot.com/2019/03/spinorama-data-revel-ultima.html
--- datas/metadata.py
+++ datas/metadata.py
@@ -771,6 +771,19 @@ speakers_info = {
             'format': 'webplotdigitizer',
             }],
     },
+    'Revel Ultima Salon': {
+        'brand': 'Revel',
+        'model': 'Ultima Salon',
+        'type': 'passive',
+        'price': '',
+        'shape': 'floorstander',
+        'amount': 'pair',
+        'measurements': [{
+            'origin': 'Vendors/Revel',
+            'website': 'www.revel.com',
+            'format': 'webplotdigitizer',
+            }],
+    },
     'Sanders Sound Systems Model 11': {
         'brand': 'Sanders Sound Systems',
         'model': 'Model 11',
