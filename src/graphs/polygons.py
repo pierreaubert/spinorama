fc5aaf96c54322418050523370869b6b364c1e5a
Pierre Aubert <pierreaubert@yahoo.fr>
2020-05-25 21:02:33 +0200

    add polygons to optimise isobands

---
+++ src/graphs/polygons.py
@@ -0,0 +1,96 @@
+def is_close(poly):
+    # if polygon is closed then return 1 else return 0
+    if len(poly)>2 and poly[0] == poly[-1]:
+        return 1
+    return 0
+
+
+def order_polygon(poly, segment):
+    # return the same polygon but ordered starting by segment
+    ordered = []
+    closed = is_close(poly)
+    for i, p in enumerate(poly):
+        if p == segment[0]:
+            j = i+1
+            if i == len(poly)-1:
+                j = 0
+            # print(poly, j)
+            if poly[j] == segment[1]:
+                o1 = [poly[k] for k in range(i, len(poly)-closed)]
+                o2 = [poly[k] for k in range(0, i)]
+                ordered = o1 + o2
+                if closed == 1:
+                    ordered.append(ordered[0])
+                #    print('debug case 1: {0} + {1} + {2}'.format(o1, o2, ordered[0]))
+                #else:
+                #    print('debug case 1: {0} + {1} + '.format(o1, o2))
+                return ordered
+            j = i-1
+            if i == 0:
+                j = len(poly)-1
+            # print(poly, j)
+            if poly[j] == segment[1]:
+                o1 = [poly[k] for k in range(i, -1+closed, -1)]
+                o2 = [poly[k] for k in range(len(poly)-1, i, -1)]
+                ordered = o1 + o2
+                if closed == 1:
+                    ordered.append(ordered[0])
+                #    print('debug case 2: {0} + {1} + {2}'.format(o1, o2, ordered[0]))
+                #else:
+                #    print('debug case 2: {0} + {1} + '.format(o1, o2))
+                return ordered
+    return None
+
+
+def merge_2polygons(poly1, poly2, segment):
+    # return a merged polygon from the 2 polygons that share a common segment
+    print('debug p1={0} p2={1} on {2}'.format(poly1, poly2, segment))
+    o1 = order_polygon(poly1, segment)
+    c1 = is_close(poly1)
+    o2 = order_polygon(poly2, segment)
+    c2 = is_close(poly2)
+    print('debug o1={0} o2={1}'.format(o1, o2))
+    m1 = [o1[k] for k in range(1, len(o1)-c1)] + [o1[0]]
+    m2 = [o2[k] for k in range(len(o2)-1-c2, 0, -1)]
+    print('debug m1={0} + m2={1} + c={2}'.format(m1, m2, m1[0]))
+    return m1 + m2 
+
+
+def merge_connected_polygons(isoband):
+    common = {}
+    for i, polygon in enumerate(isoband):
+        for point in range(0, len(polygon)-1):
+            segment = (polygon[point][0], polygon[point][1], polygon[point+1][0], polygon[point+1][1])
+            swapped = (segment[2], segment[3], segment[0], segment[1])
+            if segment in common:
+                common[segment].append(i)
+            elif swapped in common:
+                common[swapped].append(i)
+            else:
+                common[segment] = [i]
+
+    count_common = len([1 for c in common.keys() if len(common[c])>1])
+    if count_common == 0:
+        return isoband
+
+    
+    new_isoband = []
+    index_of_polygons = [i for i in range(0, len(isoband))]
+    set_of_polygons = set(index_of_polygons)
+    for segment in common.keys():
+        polygons = common[segment]
+        if len(polygons) == 2:
+            if polygons[0] in set_of_polygons and polygons[1] in set_of_polygons:
+                poly1 = isoband[polygons[0]]
+                poly2 = isoband[polygons[1]]
+                new_isoband.append(merge_2polygons(poly1, poly2,
+                                                [(segment[0], segment[1]),
+                                                 (segment[2], segment[3])]))
+                set_of_polygons.remove(polygons[0])
+                set_of_polygons.remove(polygons[1])
+
+    for p in set_of_polygons:
+        new_isoband.append(isoband[p])
+    
+    # need to iterate
+    return new_isoband
---
+++ tests/test_polygons.py
@@ -0,0 +1,136 @@
+import unittest
+import logging
+import numpy as np
+import pandas as pd
+
+import graphs.polygons as poly
+
+p0 = (0,0)    
+p1 = (1,0)
+p2 = (1,1)
+p3 = (0,1)
+p4 = (-1,1)
+p5 = (-1,0)
+
+class PolyIsCloseTests(unittest.TestCase):
+
+    def setUp(self):
+        self.po = [p0, p1, p2]
+        self.pc = [p0, p1, p2, p0]
+    
+    def test_is_close(self):
+        self.assertEqual(poly.is_close(self.po), 0)
+        self.assertEqual(poly.is_close(self.pc), 1)
+
+class PolyOrderTests(unittest.TestCase):
+
+    def setUp(self):
+        self.triangles = ([p0, p1, p2], [p0, p2, p1], [p2, p1, p0], [p2, p0, p1], [p1, p0, p2], [p1, p2, p0])
+        self.rectangles = ([p0, p1, p2, p3], [p0, p3, p2, p1],
+                           [p1, p2, p3, p0], [p1, p0, p3, p2],
+                           [p2, p3, p0, p1], [p2, p1, p0, p3],
+                           [p3, p0, p1, p2], [p3, p2, p1, p0])
+    
+    def test_ordering_triangles(self):
+        print('--- test open triangles ---')
+        for triangle in self.triangles:
+            self.assertEqual(poly.order_polygon(triangle, [p0, p1]), [p0, p1, p2])
+            self.assertEqual(poly.order_polygon(triangle, [p0, p2]), [p0, p2, p1])
+            self.assertEqual(poly.order_polygon(triangle, [p1, p2]), [p1, p2, p0])
+            self.assertEqual(poly.order_polygon(triangle, [p1, p0]), [p1, p0, p2])
+            self.assertEqual(poly.order_polygon(triangle, [p2, p0]), [p2, p0, p1])
+            self.assertEqual(poly.order_polygon(triangle, [p2, p1]), [p2, p1, p0])
+
+        print('--- test closed triangles ---')
+        for triangle in self.triangles:
+            triangle_close = triangle
+            triangle_close.append(triangle[0])
+            self.assertEqual(poly.order_polygon(triangle_close, [p0, p1]), [p0, p1, p2, p0])
+            self.assertEqual(poly.order_polygon(triangle_close, [p0, p2]), [p0, p2, p1, p0])
+            self.assertEqual(poly.order_polygon(triangle_close, [p1, p2]), [p1, p2, p0, p1])
+            self.assertEqual(poly.order_polygon(triangle_close, [p1, p0]), [p1, p0, p2, p1])
+            self.assertEqual(poly.order_polygon(triangle_close, [p2, p0]), [p2, p0, p1, p2])
+            self.assertEqual(poly.order_polygon(triangle_close, [p2, p1]), [p2, p1, p0, p2])
+
+    def test_ordering_rectangles(self):
+        print('--- test open rectangles ---')
+        for rectangle in self.rectangles:
+            self.assertEqual(poly.order_polygon(rectangle, [p0, p1]), [p0, p1, p2, p3])
+            self.assertEqual(poly.order_polygon(rectangle, [p1, p0]), [p1, p0, p3, p2])
+            self.assertEqual(poly.order_polygon(rectangle, [p1, p2]), [p1, p2, p3, p0])
+            self.assertEqual(poly.order_polygon(rectangle, [p2, p1]), [p2, p1, p0, p3])
+            self.assertEqual(poly.order_polygon(rectangle, [p3, p2]), [p3, p2, p1, p0])
+            self.assertEqual(poly.order_polygon(rectangle, [p2, p3]), [p2, p3, p0, p1])
+            self.assertEqual(poly.order_polygon(rectangle, [p3, p0]), [p3, p0, p1, p2])
+            self.assertEqual(poly.order_polygon(rectangle, [p0, p3]), [p0, p3, p2, p1])
+
+        print('--- test closed rectangless ---')
+        for rectangle in self.rectangles:
+            rectangle_close = rectangle
+            rectangle_close.append(rectangle[0])
+            self.assertEqual(poly.order_polygon(rectangle_close, [p0, p1]), [p0, p1, p2, p3, p0])
+            self.assertEqual(poly.order_polygon(rectangle_close, [p1, p0]), [p1, p0, p3, p2, p1])
+            self.assertEqual(poly.order_polygon(rectangle_close, [p1, p2]), [p1, p2, p3, p0, p1])
+            self.assertEqual(poly.order_polygon(rectangle_close, [p2, p1]), [p2, p1, p0, p3, p2])
+            self.assertEqual(poly.order_polygon(rectangle_close, [p3, p2]), [p3, p2, p1, p0, p3])
+            self.assertEqual(poly.order_polygon(rectangle_close, [p2, p3]), [p2, p3, p0, p1, p2])
+            self.assertEqual(poly.order_polygon(rectangle_close, [p3, p0]), [p3, p0, p1, p2, p3])
+            self.assertEqual(poly.order_polygon(rectangle_close, [p0, p3]), [p0, p3, p2, p1, p0])
+
+            
+class PolyMergeTests(unittest.TestCase):
+
+    def setUp(self):
+        self.triangles = [
+         ([p0, p1, p2], [p0, p2, p3], [p2, p0], [p0, p1, p2, p3, p0]),
+         ([p0, p1, p2], [p0, p2, p3], [p0, p2], [p2, p1, p0, p3, p2]),
+         ([p0, p1, p3], [p1, p2, p3], [p1, p3], [p3, p0, p1, p2, p3]),
+         ([p0, p1, p3], [p1, p2, p3], [p3, p1], [p1, p0, p3, p2, p1]),
+        ]
+        self.rectangles = [
+         ([p0, p1, p2, p3], [p0, p3, p4, p5], [p0, p3], [p3, p2, p1, p0, p5, p4, p3]),
+        ]
+        self.mixed = [
+         ([p0, p1, p2], [p2, p4, p5, p0], [p0, p2], [p2, p1, p0, p5, p4, p2]),
+         ([p0, p1, p2], [p2, p4, p5, p0], [p2, p0], [p0, p1, p2, p4, p5, p0]),
+         ([p0, p1, p2], [p2, p3, p4, p5, p0], [p0, p2], [p2, p1, p0, p5, p4, p3, p2]),
+        ]
+                            
+
+    def test_merge_triangles(self):
+        for triangle1, triangle2, segment, expected in self.triangles:
+            self.assertEqual(poly.merge_2polygons(triangle1, triangle2, segment), expected)
+            triangle1_close = triangle1
+            triangle1_close.append(triangle1[0])
+            triangle2_close = triangle2
+            triangle2_close.append(triangle2[0])
+            self.assertEqual(poly.merge_2polygons(triangle1_close, triangle2_close, segment), expected)
+
+    def test_merge_rectangles(self):
+        for rectangle1, rectangle2, segment, expected in self.rectangles:
+            self.assertEqual(poly.merge_2polygons(rectangle1, rectangle2, segment), expected)
+            rectangle1_close = rectangle1
+            rectangle1_close.append(rectangle1[0])
+            rectangle2_close = rectangle2
+            rectangle2_close.append(rectangle2[0])
+            self.assertEqual(poly.merge_2polygons(rectangle1_close, rectangle2_close, segment), expected)
+            
+    def test_merge_mixed(self):
+        for poly1, poly2, segment, expected in self.mixed:
+            self.assertEqual(poly.merge_2polygons(poly1, poly2, segment), expected)
+            poly1_close = poly1
+            poly1_close.append(poly1[0])
+            poly2_close = poly2
+            poly2_close.append(poly2[0])
+            self.assertEqual(poly.merge_2polygons(poly1_close, poly2_close, segment), expected)
+            
+class PolyMergeConnectedTests(unittest.TestCase):
+
+    def setUp(self):
+        self.datasets = [
+         ([p0, p1, p2, p0], [p0, p2, p3, p0])
+        ]
+
+    def test_merge_connected(self):
+        for data in self.datasets:
+            self.assertEqual(poly.merge_connected_polygons(data), [[p0, p1, p2, p3, p0]])
