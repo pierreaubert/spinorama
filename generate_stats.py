8eadac52231b5456e8f2394cc00101469ae2edd5
Pierre Aubert <pierreaubert@yahoo.fr>
2020-07-27 13:54:19 +0200

    add a csv output

--- generate_stats.py
+++ generate_stats.py
@@ -24,7 +24,7 @@ usage: generate_stats.py [--help] [--version] [--dev] [--print=<what>]\
 Options:
   --help            display usage()
   --version         script version number
-  --print=<what>    print information. Options are 'eq'
+  --print=<what>    print information. Options are 'eq_txt' or 'eq_csv'
   --log-level=<level> default is WARNING, options are DEBUG INFO ERROR.
 """
 import json
@@ -66,7 +66,7 @@ def meta2df(meta):
     return df
 
 
-def print_eq(speakers):
+def print_eq(speakers, txt_format):
     results = []
     for i in speakers:
         speaker = speakers[i]
@@ -75,26 +75,48 @@ def print_eq(speakers):
             pref = measurement.get('pref_rating', None)
             pref_eq = measurement.get('pref_rating_eq', None)
             if pref is not None and pref_eq is not None:
-                results.append((i, pref, pref_eq))
+                name = i
+                if key not in ('asr', 'princeton', 'eac', 'vendor'):
+                    name = '{} ({})'.format(i, key)
+                results.append((name, pref, pref_eq))
 
-    print('                                           | NBD  NBD  LFX   SM |  SCR | NBD  NBD  LFX   SM | SCR | ')
-    print('Speaker                                    |  ON  PIR   Hz  PIR |  ASR |  ON  PIR   Hz  PIR |  EQ | DIFF')
-    print('-------------------------------------------+--------------------+------+--------------------+-----+-----')
-    for i, pref, pref_eq in sorted(results, key=lambda a: -a[2]['pref_score']):
-        print('{0:42s} | {1:0.2f} {2:0.2f} {3:3.0f} {4:0.2f} | {5:+1.1f} | {6:0.2f} {7:0.2f} {8:3.0f} {9:0.2f} | {10:1.1f} | {11:+1.1f}'.format(
-            i,
-            pref['nbd_on_axis'], 
-            pref['nbd_pred_in_room'], 
-            pref['lfx_hz'], 
-            pref['sm_pred_in_room'], 
-            pref['pref_score'], 
-            pref_eq['nbd_on_axis'], 
-            pref_eq['nbd_pred_in_room'], 
-            pref_eq['lfx_hz'], 
-            pref_eq['sm_pred_in_room'], 
-            pref_eq['pref_score'],
-            pref_eq['pref_score']-pref['pref_score']
-            ))
+    if txt_format == 'txt':
+        print('                                           | NBD  NBD  LFX   SM |  SCR | NBD  NBD  LFX   SM | SCR | ')
+        print('Speaker                                    |  ON  PIR   Hz  PIR |  ASR |  ON  PIR   Hz  PIR |  EQ | DIFF')
+        print('-------------------------------------------+--------------------+------+--------------------+-----+-----')
+        for i, pref, pref_eq in sorted(results, key=lambda a: -a[2]['pref_score']):
+            print('{0:42s} | {1:0.2f} {2:0.2f} {3:3.0f} {4:0.2f} | {5:+1.1f} | {6:0.2f} {7:0.2f} {8:3.0f} {9:0.2f} | {10:1.1f} | {11:+1.1f}'.format(
+                i,
+                pref['nbd_on_axis'], 
+                pref['nbd_pred_in_room'], 
+                pref['lfx_hz'], 
+                pref['sm_pred_in_room'], 
+                pref['pref_score'], 
+                pref_eq['nbd_on_axis'], 
+                pref_eq['nbd_pred_in_room'], 
+                pref_eq['lfx_hz'], 
+                pref_eq['sm_pred_in_room'], 
+                pref_eq['pref_score'],
+                pref_eq['pref_score']-pref['pref_score']
+                ))
+    elif txt_format == 'csv':
+        print('"Speaker", "NBD", "NBD", "LFX", "SM", "SCR", "NBD", "NBD", "LFX", "SM", "SCR"')
+        print('"Speaker", "ON", "PIR", "Hz", "PIR", "ASR", "ON", "PIR", "Hz", "PIR", "EQ", "DIFF"')
+        for i, pref, pref_eq in sorted(results, key=lambda a: -a[2]['pref_score']):
+            print('"{0}", {1:0.2f}, {2:0.2f}, {3:3.0f}, {4:0.2f}, {5:+1.1f}, {6:0.2f}, {7:0.2f}, {8:3.0f}, {9:0.2f}, {10:+1.1f}, {11:+1.1f}'.format(
+                i,
+                pref['nbd_on_axis'], 
+                pref['nbd_pred_in_room'], 
+                pref['lfx_hz'], 
+                pref['sm_pred_in_room'], 
+                pref['pref_score'], 
+                pref_eq['nbd_on_axis'], 
+                pref_eq['nbd_pred_in_room'], 
+                pref_eq['lfx_hz'], 
+                pref_eq['sm_pred_in_room'], 
+                pref_eq['pref_score'],
+                pref_eq['pref_score']-pref['pref_score']
+                ))
         
 
         
@@ -196,7 +218,7 @@ def generate_stats(meta):
 
 if __name__ == '__main__':
     args = docopt(__doc__,
-                  version='generate_stats.py version 0.1',
+                  version='generate_stats.py version 0.2',
                   options_first=True)
 
     print_what = None
@@ -232,8 +254,12 @@ if __name__ == '__main__':
     logging.warning('Data {0} loaded ({1} speakers)!'.format(json_filename, len(meta)))
 
     if print_what is not None:
-        if print_what == 'eq':
-            print_eq(meta)
+        if print_what == 'eq_txt':
+            print_eq(meta, 'txt')
+        elif  print_what == 'eq_csv':
+            print_eq(meta, 'csv')
+        else:
+            logging.error('unkown print type either "eq_txt" or "eq_csv"')
     else:
         generate_stats(meta)
 
