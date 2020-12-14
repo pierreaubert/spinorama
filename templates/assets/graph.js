a34d9315b2e5cb45b28648846f9c71ac72bbcdab
Pierre F. Aubert <pierre0aubert@gmail.com>
2020-12-14 21:24:38 +0100

    added extra filtering options

--- templates/assets/graph.js
+++ templates/assets/graph.js
@@ -1,38 +1,38 @@
 function displayGraph(spec, divName) {
-    
-    async function run() {
-	const config = {
-            // default view background color
-            // covers the entire view component
-            background: "#efefef",
-            axis: {
-		labelFont: "serif",
-		labelFontSize: 14,
-		tickWidth: 3,
-            },
-	};
-	
-	function patch(spec) {
-	    width = window.innerWidth-180;
-	    spec.width = width;
-	    spec.height = width/2-180;
-	    return spec;
+
+	async function run() {
+		const config = {
+			// default view background color
+			// covers the entire view component
+			background: "#efefef",
+			axis: {
+				labelFont: "serif",
+				labelFontSize: 14,
+				tickWidth: 3,
+			},
+		};
+
+		function patch(spec) {
+			var width = window.innerWidth - 180;
+			spec.width = width;
+			spec.height = width / 2 - 180;
+			return spec;
+		}
+
+		const result = await vegaEmbed(divName, spec, {
+			config: config,
+			tooltip: { theme: "dark" },
+			patch: patch
+		}).then(
+			function (result) {
+				// width = window.innerWidth-300;
+				// result.view.width(width);
+				// result.view.height(width/2);
+				return result;
+			}
+		);
+
+		// console.log(window.innerWidth);
 	}
-	
-	const result = await vegaEmbed(divName, spec, {
-            config: config,
-            tooltip: { theme: "dark" },
-	    patch: patch
-	}).then(
-	    function(result) {
-	      // width = window.innerWidth-300;
-		// result.view.width(width);
-		// result.view.height(width/2);
-		return result;
-	    }
-	);
-	
-	// console.log(window.innerWidth);
-    }
-    run();
+	run();
 }
--- templates/assets/search.js
+++ templates/assets/search.js
@@ -1,80 +1,147 @@
-window.$ = window.jQuery;
+Window.$ = window.jQuery;
 
 $(document).ready(function () {
 
-    window.$.getJSON('${site}/assets/metadata.json', function (response) {
-	console.log('got json')
-	const metadata = Object.values(response)
-	console.log('got metadata')
-	const fuse = new Fuse(metadata, {
-	    isCaseSensitive: false,
-	    matchAllTokens: true,
-	    findAllMatches: true,
-	    minMatchCharLength: 2,
-	    keys: ['brand', 'model', 'type', 'shape',
-		   'measurements.asr.origin', 'measurements.vendor.origin', 'measurements.printeton.origin', 'measurements.eac.origin'
-		  ],
-	    treshhold: 0.1,
-	    distance: 2,
-	    includeScore: true,
-	    useExtendedSearch: true
-	})
-	
-	console.log('starting search')
-	
-	$('#searchInput').on('keyup', function () {
-	    const resultdiv = $('div.searchresults')
-	    const keywords = $(this).val()
-	    console.log('searching: '+keywords)
-	    if (keywords.length === 0) {
-		for (const item in metadata) {
-		    const id = (metadata[item].brand + '-' + metadata[item].model).replace(/['.+& ]/g, '-')
-		    $('#' + id).show()
+	window.$.getJSON("${site}/assets/metadata.json", function (response) {
+		// console.log("got json")
+		const metadata = Object.values(response);
+		// console.log("got metadata")
+		const fuse = new Fuse(metadata, {
+			isCaseSensitive: false,
+			matchAllTokens: true,
+			findAllMatches: true,
+			minMatchCharLength: 2,
+			keys: ["brand", "model"],
+			treshhold: 0.1,
+			distance: 2,
+			includeScore: true,
+			useExtendedSearch: true
+		});
+
+		const resultdiv = $("div.searchresults");
+
+		var filter = {
+			reviewer: "",
+			shape: "",
+			power: "",
+		};
+
+		function selectDispatch(filter) {
+			var keywords = $("#searchInput").val();
+			// console.log("keywords: "+keywords)
+			if (keywords === "") {
+				// console.log("display filter");
+				display_filter(resultdiv, metadata, filter);
+			} else {
+				// console.log("display search");
+				var results = fuse.search(keywords);
+				display_search(resultdiv, results, filter);
+			}
 		}
-		resultdiv.show()
-	    } else {
-		const result = fuse.search(keywords)
-		if (result.length === 0) {
-		    resultdiv.hide()
-		} else {
-		    for (const item in metadata) {
-			const id = (metadata[item].brand + '-' + metadata[item].model).replace(/['.+& ]/g, '-')
-			console.log('hide:'+id);
-			$('#' + id).hide()
-		    }
-		    let minScore = 1
-		    for (const item in result) {
-			if (result[item].score < minScore) {
-			    minScore = result[item].score
+
+		$("#selectReviewer").on("change", function () {
+			// console.log("select reviewer");
+			filter["reviewer"] = this.value;
+			selectDispatch(filter);
+		});
+
+		$("#selectShape").on("change", function () {
+			filter["shape"] = this.value;
+			selectDispatch(filter);
+		});
+
+		$("#selectPower").on("change", function () {
+			filter["power"] = this.value;
+			selectDispatch(filter);
+		});
+
+		$("#searchInput").on("keyup", function () {
+			var keywords = $(this).val();
+			// console.log("search start "+keywords);
+			var results = fuse.search(keywords);
+			display_search(resultdiv, results, filter);
+		});
+
+		function is_filtered(item, filter) {
+			var show = true;
+			var default_measurement = item["default_measurement"];
+			var origin = item["measurements"][default_measurement]["origin"];
+			if (filter.reviewer !== "" && origin !== filter.reviewer) {
+				show = false;
 			}
-		    }
-		    console.log('minscore '+minScore)
-		    if (minScore < Math.pow(10,-15)) {
-			for (const item in result) {
-			    const id = (result[item].item.brand + '-' + result[item].item.model).replace(/['.+& ]/g, '-')
-			    if (result[item].score < Math.pow(10,-15)) {
-				$('#' + id).show()
-				console.log('perfect match:'+id+' minscore:' + minScore+' score:' + result[item].score);
-			    } else {
-				console.log('skip match:'+id+' minscore:' + minScore+' score:' + result[item].score);
-			    }
+			if (filter.power !== "" && item.type !== filter.power) {
+				show = false;
 			}
-		    } else {
-			for (const item in result) {
-			    const id = (result[item].item.brand + '-' + result[item].item.model).replace(/['.+& ]/g, '-')
-			    if (result[item].score === minScore) {
-				console.log('show:'+id+' minscore:' + minScore+' score:' + result[item].score);
-				$('#' + id).show()
-			    } else {
-				console.log('skip partial match:'+id+' minscore:' + minScore+' score:' + result[item].score);
-			    }
+			if (filter.shape !== "" && item.shape !== filter.shape) {
+				show = false;
 			}
-		    }
-		    resultdiv.show()
+			return show;
 		}
-	    }
-	})
-    }).fail( function(jqXHR, textStatus, errorThrown) {
-	console.log('getJSON request failed! ' + textStatus);
-    })
-})
+
+		function display_filter(resultdiv, meta, filter) {
+			// console.log("display filter start");
+			for (const item in meta) {
+				var show = is_filtered(meta[item], filter);
+				const id = (meta[item].brand + "-" + meta[item].model).replace(/['.+& ]/g, "-");
+				if (show) {
+					$("#" + id).show();
+				} else {
+					$("#" + id).hide();
+				}
+			}
+			resultdiv.show();
+		}
+
+		function display_search(resultdiv, results, filter) {
+			// console.log("display search start");
+			if (results.length === 0) {
+				display_filter(resultdiv, metadata, filter);
+				return;
+			}
+			// hide all
+			for (const item in metadata) {
+				const id = (metadata[item].brand + "-" + metadata[item].model).replace(/['.+& ]/g, "-");
+				$("#" + id).hide();
+			}
+			// minScore
+			let minScore = 1;
+			for (const item in results) {
+				if (results[item].score < minScore) {
+					minScore = results[item].score;
+				}
+			}
+			// console.log("minScore is "+minScore);
+			for (var item in results) {
+				var show = true;
+				var result = results[item];
+				var meta = result.item;
+				var score = result.score;
+				// console.log("evaluating "+meta.brand+" "+meta.model);
+				if (show && !is_filtered(meta, filter)) {
+					show = false;
+				}
+				if (show) {
+					if (minScore < Math.pow(10, -15) && score >= Math.pow(10, -15)) {
+						show = false;
+					} else {
+						if (score !== minScore) {
+							show = false;
+						}
+					}
+				}
+				const id = (meta.brand + "-" + meta.model).replace(/[".+& ]/g, "-");
+				if (show) {
+					$("#" + id).show();
+				} else {
+					$("#" + id).hide();
+				}
+
+			}
+			resultdiv.show();
+		}
+
+	}).fail(function (jqXHR, textStatus) {
+		console.log("getJSON request failed! " + textStatus);
+	});
+
+});
