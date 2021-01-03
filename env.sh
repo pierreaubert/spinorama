861592aa2df0f326fbeb1c5210a54c1a90e34d26
pierreaubert <pierre0aubert@gmail.com>
2021-01-03 12:33:52 +0000

    cosmetic changes

--- env.sh
+++ env.sh
@@ -24,10 +24,10 @@ fi
 
 ## Github keys
 ## ----------------------------------------------------------------------
-github=$(ssh-add -l | grep github)
+github=$(ssh-add -l | grep github | cut -d ' ' -f 3)
 if test -z $github; then
     ssh-add ~/.ssh/id_rsa_github 2>&1 >> env.log
-    github=$(ssh-add -l 2>&1 | grep github)
+    github=$(ssh-add -l 2>&1 | grep github | cut -d ' ' -f 3)
 fi
 
 ## python virtualenv
@@ -57,9 +57,9 @@ fi
 ## summary
 ## ----------------------------------------------------------------------
 echo 'SPIN          ' $SPIN
-echo '  python3     ' $(python3 --version) $(which python3)
-echo '  pip3        ' $(which pip3)
-echo '  jupyter-lab ' $(which jupyter-lab)
+echo '  '$(python3 --version) $(which python3)
+echo '  '$(pip3 -V)
+echo '  jupyter-lab ' $(jupyter-lab --version) $(which jupyter-lab)
 echo '  PYTHONPATH  ' $PYTHONPATH
 echo '  github key  ' $github
 echo '  GPU         ' $GPU
