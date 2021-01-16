e00dee84c453731be78d451ba9cf38b372e0f08c
pierreaubert <pierre0aubert@gmail.com>
2021-01-16 13:38:44 +0000

    add a function to compare 3 algos for 1 speaker

--- src/spinorama/filter_scores.py
+++ src/spinorama/filter_scores.py
@@ -1,18 +1,12 @@
 #                                                  -*- coding: utf-8 -*-
-import math
 import logging
-import numpy as np
-import pandas as pd
-import scipy.signal as sig
-import altair as alt
 
 from .load import graph_melt
-from .graph import graph_spinorama, graph_freq
 from .compute_scores import speaker_pref_rating, nbd
-from .compute_cea2034 import compute_cea2034, estimated_inroom_HV, spl2pressure, pressure2spl, listening_window
-from .load_parse import load_normalize
-from .filter_iir import Biquad
-from .filter_peq import peq_build, peq_apply_measurements, peq_print
+from .compute_cea2034 import compute_cea2034, estimated_inroom_HV,\
+    listening_window
+from .filter_peq import peq_apply_measurements
+from .graph import graph_spinorama
 
 
 def scores_apply_filter(df_speaker, peq):
@@ -24,7 +18,7 @@ def scores_apply_filter(df_speaker, peq):
     splV_filtered = peq_apply_measurements(splV, peq)
     # compute filtered score
     spin_filtered = graph_melt(compute_cea2034(splH_filtered, splV_filtered))
-    pir_filtered  = graph_melt(estimated_inroom_HV(splH_filtered, splV_filtered))
+    pir_filtered = graph_melt(estimated_inroom_HV(splH_filtered, splV_filtered))
     score_filtered = speaker_pref_rating(spin_filtered, pir_filtered, rounded=False)
     if score_filtered is None:
         logging.info('computing pref score for eq failed')
@@ -52,6 +46,29 @@ def scores_print(score, score_filtered):
     print('-----------------')
 
 
+def scores_print2(score, score1, score2):
+    print('         SPK  S1  S2')
+    print('----------------------')
+    print('NBD  ON {0:0.2f} {1:0.2f} {2:0.2f}'.format(
+        score['nbd_on_axis'], score1['nbd_on_axis'], score2['nbd_on_axis']))
+    print('NBD  LW {0:0.2f} {1:0.2f} {2:0.2f}'.format(
+        score['nbd_listening_window'], score1['nbd_listening_window'], score2['nbd_listening_window']))
+    print('NBD PIR {0:0.2f} {1:0.2f} {2:0.2f}'.format(
+        score['nbd_pred_in_room'], score1['nbd_pred_in_room'], score2['nbd_pred_in_room']))
+    print('SM  PIR {0:0.2f} {1:0.2f} {2:0.2f}'.format(
+        score['sm_pred_in_room'], score1['sm_pred_in_room'], score2['sm_pred_in_room']))
+    print('SM   SP {0:0.2f} {1:0.2f} {2:0.2f}'.format(
+        score['sm_sound_power'], score1['sm_sound_power'], score2['sm_sound_power']))
+    print('LFX       {0:0.0f}   {1:0.0f}   {2:0.0f}'.format(
+        score['lfx_hz'], score1['lfx_hz'], score2['lfx_hz']))
+    print('LFQ     {0:0.2f} {1:0.2f} {2:0.2f}'.format(
+        score['lfq'], score1['lfq'], score2['lfq']))
+    print('----------------------')
+    print('Score    {0:0.1f}  {1:0.1f}  {2:0.1f}'.format(
+        score['pref_score'], score1['pref_score'], score2['pref_score']))
+    print('----------------------')
+
+
 def scores_loss(df_speaker, peq):
     # optimise for score directly
     _, _, score_filtered = scores_apply_filter(df_speaker, peq)
@@ -73,5 +90,3 @@ def lw_loss(df_speaker, peq):
     score = nbd(lw_filtered)
     print('LW score: {}'.format(score))
     return score
-
-
