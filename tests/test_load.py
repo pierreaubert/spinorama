742e9802bfa50557a27dcaa6a92f998905b21d11
pierreaubert <pierre0aubert@gmail.com>
2021-02-24 07:57:14 +0000

    add LICENSE to test files

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,3 +1,22 @@
+#!/usr/bin/env python3
+#                                                  -*- coding: utf-8 -*-
+# A library to display spinorama charts
+#
+# Copyright (C) 2020-2021 Pierre Aubert pierreaubert(at)yahoo(dot)fr
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import unittest
 
 from spinorama.load import sort_angles
