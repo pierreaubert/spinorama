f427c4a1ea3da806beaf7fb11462bb6b6b8f7eae
pierreaubert <pierre0aubert@gmail.com>
2021-03-04 13:28:42 +0000

    fix radar plot if normalized

--- src/spinorama/graph_radar.py
+++ src/spinorama/graph_radar.py
@@ -4,6 +4,9 @@ import math
 import numpy as np
 import pandas as pd
 
+from .load import sort_angles
+
+
 logger = logging.getLogger("spinorama")
 
 
@@ -71,9 +74,9 @@ def label(i):
     return "{:d} Hz".format(i)
 
 
-def grid_grid(anglelist):
+def grid_grid(circle, anglelist):
     # to limit the annoying effect of all lines crossing at 0,0
-    radius = 0.25
+    radius = 0.25 * circle
     grid0 = [
         (radius * math.cos(p * math.pi / 180), radius * math.sin(p * math.pi / 180))
         for p in anglelist
@@ -92,7 +95,7 @@ def grid_grid(anglelist):
     return pd.DataFrame({"x": gridX, "y": gridY})
 
 
-def grid_circle(anglelist, dbmax):
+def grid_circle(radius, anglelist, dbmax):
     circleX = []
     circleY = []
     circleL = []
@@ -100,10 +103,10 @@ def grid_circle(anglelist, dbmax):
     legendY = []
     legendT = []
     for c in [0.25, 0.5, 0.75, 1]:
-        X, Y = circle(c, anglelist)
+        X, Y = circle(c * radius, anglelist)
         circleX.append(X)
         circleY.append(Y)
-        g_label = dbmax * (1 - c)
+        g_label = dbmax * (1 - c) * radius
         circleL.append([g_label for i in range(0, len(X))])
         legendX.append(X[0])
         legendY.append(Y[0])
@@ -125,8 +128,8 @@ def grid_circle(anglelist, dbmax):
     return circles, text
 
 
-def grid_text(anglelist):
-    textX, textY = circle(1.02, anglelist)
+def grid_text(radius, anglelist):
+    textX, textY = circle(1.02 * radius, anglelist)
     textT = [angle2str(a) for a in anglelist]
     if join(anglelist):
         textT += [""]
@@ -137,7 +140,7 @@ def find_nearest_freq(dfu, hz, tolerance=0.05):
     """ return the index of the nearest freq in dfu, return None if not found """
     ihz = None
     for i in dfu.index:
-        f = dfu.loc[i, "Freq"]
+        f = dfu.loc[i]
         if abs(f - hz) < hz * tolerance:
             ihz = i
             break
@@ -145,30 +148,39 @@ def find_nearest_freq(dfu, hz, tolerance=0.05):
     return ihz
 
 
-def plot(anglelist, dfu):
+def plot(anglelist, df):
+    dfu = sort_angles(df)
+    db_mean = np.mean(dfu.loc[(dfu.Freq > 900) & (dfu.Freq < 1100)]["On Axis"].values)
+    freq = dfu.Freq
+    dfu = dfu.drop("Freq", axis=1)
+    db_min = np.min(dfu.min(axis=0).values)
+    db_max = np.max(dfu.max(axis=0).values)
+    db_scale = max(abs(db_max), abs(db_min))
+    if db_mean < 45:
+        dfu += db_scale
+    print(db_min, db_max, db_mean, db_scale)
     # build 3 plots
     dbX = []
     dbY = []
     hzZ = []
-    db_max_onaxis = dfu["On Axis"].max() - 100
     for hz in [100, 500, 1000, 10000, 15000]:
-        ihz = find_nearest_freq(dfu, hz)
+        ihz = find_nearest_freq(freq, hz)
         if ihz is None:
             continue
-        X, Y, Z = projection(anglelist, dfu.loc[ihz][1:] - db_max_onaxis, hz)
+        X, Y, Z = projection(
+            anglelist, dfu.loc[ihz][dfu.columns != "Freq"] / db_scale, hz
+        )
         # add to global variable
         dbX.append(X)
         dbY.append(Y)
         hzZ.append(Z)
 
     # normalise
-    # db_min = min(np.array(dbX).min(), np.array(dbY).min())
-    db_max = max(np.array(dbX).max(), np.array(dbY).max())
-    dbX = [v2 / db_max for v1 in dbX for v2 in v1]
-    dbY = [v2 / db_max for v1 in dbY for v2 in v1]
-    # print('dbX min={} max={}'.format(np.array(dbX).min(), np.array(dbX).max()))
-    # print('dbY min={} max={}'.format(np.array(dbY).min(), np.array(dbY).max()))
+    dbX = [v2 for v1 in dbX for v2 in v1]
+    dbY = [v2 for v1 in dbY for v2 in v1]
+    print("dbX min={} max={}".format(np.array(dbX).min(), np.array(dbX).max()))
+    print("dbY min={} max={}".format(np.array(dbY).min(), np.array(dbY).max()))
 
     hzZ = [label(i2) for i1 in hzZ for i2 in i1]
 
-    return db_max, pd.DataFrame({"x": dbX, "y": dbY, "Freq": hzZ})
+    return db_mean, pd.DataFrame({"x": dbX, "y": dbY, "Freq": hzZ})
