fix corner case in lfx computation

src/spinorama/compute_scores.py
@@ -95,14 +95,21 @@ def lfx(lw, sp) -> float:
     the loudspeaker, particularly speakers that have rear-firing ports.
     """
     lw_ref = np.mean(lw.loc[(lw.Freq >= 300) & (lw.Freq <= 10000)].dB) - 6
+    logger.debug("lw_ref {}".format(lw_ref))
     # find first freq such that y[freq]<y_ref-6dB
     lfx_range = sp.loc[(sp.Freq < 300) & (sp.dB <= lw_ref)].Freq
-    lfx_grouped = consecutive_groups(lfx_range.iteritems(), lambda x: x[0])
-    try:
-        lfx_hz = list(next(lfx_grouped))[-1][1]
-    except Exception:
-        lfx_hz = -1.0
-        logger.debug("lfx: selecting max {0}".format(lfx_hz))
+    if len(lfx_range.values) == 0:
+        # happens with D&D 8C when we do not have a point low enough to get the -6
+        lfx_hz = sp.Freq.values[0]
+    else:
+        lfx_grouped = consecutive_groups(lfx_range.iteritems(), lambda x: x[0])
+        # logger.debug('lfx_grouped {}'.format(lfx_grouped))
+        try:
+            lfx_hz = list(next(lfx_grouped))[-1][1]
+        except Exception:
+            lfx_hz = -1.0
+            logger.error("lfx: selecting max {0}".format(lfx_hz))
+    logger.debug("lfx_hz {}".format(lfx_hz))
     return math.log10(lfx_hz)
 
 