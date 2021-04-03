17c72ad648b9271b52c3289951229246a641d1bb
pierreaubert <pierre0aubert@gmail.com>
2021-04-03 11:53:16 +0000

    lint a few files v2

--- check_website.py
+++ check_website.py
@@ -6,14 +6,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-class CORSRequestHandler(SimpleHTTPRequestHandler):
-    """ Generate CORS headers """
-
-    def end_headers(self):
-        self.send_header("Access-Control-Allow-Origin", "*")
-        self.send_header("Access-Control-Allow-Methods", "GET")
-        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate")
-        return super(CORSRequestHandler, self).end_headers()
+# class CORSRequestHandler(SimpleHTTPRequestHandler):
+#    """ Generate CORS headers """
+#
+#    def end_headers(self):
+#        self.send_header("Access-Control-Allow-Origin", "*")
+#        self.send_header("Access-Control-Allow-Methods", "GET")
+#        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate")
+#        return super(CORSRequestHandler, self).end_headers()
 
 
 class SpinoramaTest(unittest.TestCase):
