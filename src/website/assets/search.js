5269a117c9f3465935beb466610b7b0ec8e8e2a5
pierreaubert <pierre0aubert@gmail.com>
2021-11-03 07:11:46 +0100

    remove a debug line

--- src/website/assets/search.js
+++ src/website/assets/search.js
@@ -3,40 +3,40 @@ Window.$ = window.jQuery;
 $(document).ready(function () {
 
     window.$.getJSON("${site}/assets/metadata.json", function (response) {
-	// console.log("got json");
-	let metadata = Object.values(response);
+        // console.log("got json");
+        let metadata = Object.values(response);
         // build a hash map
         let indirect = {};
         for (item in metadata) {
             key = (metadata[item].brand+'-'+metadata[item].model).replace(/['.+& ]/g, "-");
             indirect[key] = item;
-            console.log('adding '+key);
+            // console.log('adding '+key);
         }
-	// console.log("got metadata");
-	let resultdiv = $("div.searchresults");
+        // console.log("got metadata");
+        let resultdiv = $("div.searchresults");
 
-	let filter = {
+        let filter = {
 	    reviewer: "",
 	    shape: "",
 	    power: "",
 	    brand: "",
 	    quality: "",
-	};
+        };
 
-	let sorter = {
+        let sorter = {
 	    by: "",
-	};
+        };
 
-	function selectDispatch(filter, sorter) {
+        function selectDispatch(filter, sorter) {
 	    let keywords = $("#searchInput").val();
 	    // console.log("keywords: "+keywords);
             sort_metadata(sorter);
 
 	    if (keywords === "") {
-		// console.log("display filter");
-		display_filter(resultdiv, metadata, filter);
+                // console.log("display filter");
+                display_filter(resultdiv, metadata, filter);
 	    } else {
-		// console.log("display search");
+                // console.log("display search");
 	        const fuse = new Fuse(metadata, {
 	            isCaseSensitive: false,
 	            matchAllTokens: true,
@@ -48,47 +48,47 @@ $(document).ready(function () {
 	            includeScore: true,
 	            useExtendedSearch: true
 	        });
-		let results = fuse.search(keywords);
-		display_search(resultdiv, metadata, results, filter);
+                let results = fuse.search(keywords);
+                display_search(resultdiv, metadata, results, filter);
 	    }
 	    resultdiv.show();
-	}
+        }
 
-	$("#selectReviewer").on("change", function () {
+        $("#selectReviewer").on("change", function () {
 	    filter["reviewer"] = this.value;
 	    selectDispatch(filter, sorter);
-	});
+        });
 
-	$("#selectQuality").on("change", function () {
+        $("#selectQuality").on("change", function () {
 	    filter["quality"] = this.value;
 	    selectDispatch(filter, sorter);
-	});
+        });
 
-	$("#selectShape").on("change", function () {
+        $("#selectShape").on("change", function () {
 	    filter["shape"] = this.value;
 	    selectDispatch(filter, sorter);
-	});
+        });
 
-	$("#selectPower").on("change", function () {
+        $("#selectPower").on("change", function () {
 	    filter["power"] = this.value;
 	    selectDispatch(filter, sorter);
-	});
+        });
 
-	$("#selectBrand").on("change", function () {
+        $("#selectBrand").on("change", function () {
 	    filter["brand"] = this.value;
 	    selectDispatch(filter, sorter);
-	});
+        });
 
-	$("#sortBy").on("change", function () {
+        $("#sortBy").on("change", function () {
 	    sorter["by"] = this.value;
 	    selectDispatch(filter, sorter);
-	});
+        });
 
-	$("#searchInput").on("keyup", function () {
+        $("#searchInput").on("keyup", function () {
 	    selectDispatch(filter, sorter);
-	});
+        });
 
-	function is_filtered(item, filter) {
+        function is_filtered(item, filter) {
 	    let show = true;
 	    if (filter.reviewer !== "" ) {
                 let found = true;
@@ -97,7 +97,7 @@ $(document).ready(function () {
                     let origin = measurement["origin"].toLowerCase();
                     // console.log("debug: name="+name+" origin="+origin+" filter.reviewer="+filter.reviewer);
                     if (name.toLowerCase().endsWith(filter.reviewer.toLowerCase()) || origin == filter.reviewer.toLowerCase()) {
-			found = false;
+                        found = false;
                         break;
                     }
                 }
@@ -123,65 +123,65 @@ $(document).ready(function () {
 	    }
             // console.log("debug: name="+name+" post quality "+show)
 	    if (filter.power !== "" && item.type !== filter.power) {
-		show = false;
+                show = false;
 	    }
             // console.log("debug: name="+name+" post power "+show)
 	    if (filter.shape !== "" && item.shape !== filter.shape) {
-		show = false;
+                show = false;
 	    }
             // console.log("debug: name="+name+" post shape "+show)
 	    if (filter.brand !== "" && item.brand.toLowerCase() !== filter.brand.toLowerCase()) {
-		show = false;
+                show = false;
 	    }
             // console.log("debug: name="+name+" post brand "+show)
 	    return show;
-	}
+        }
 
-	function display_filter(resultdiv, sorted_meta, filter) {
+        function display_filter(resultdiv, sorted_meta, filter) {
 	    // console.log("display filter start #" + meta.length);
 	    for (const item in sorted_meta) {
-		let show = is_filtered(sorted_meta[item], filter);
-		const id = (sorted_meta[item].brand + "-" + sorted_meta[item].model).replace(/['.+& ]/g, "-");
-		if (show) {
+                let show = is_filtered(sorted_meta[item], filter);
+                const id = (sorted_meta[item].brand + "-" + sorted_meta[item].model).replace(/['.+& ]/g, "-");
+                if (show) {
                     // console.log(sorted_meta[item].brand + "-"+ sorted_meta[item].model + " is shown");
 		    $("#" + id).show();
-		} else {
+                } else {
                     // console.log(sorted_meta[item].brand + "-"+ sorted_meta[item].model + " is filtered");
 		    $("#" + id).hide();
-		}
+                }
 	    }
-	}
+        }
 
-	function display_search(resultdiv, sorted_meta, results, filter) {
+        function display_search(resultdiv, sorted_meta, results, filter) {
 	    // console.log("---------- display search start ----------------");
 	    if (results.length === 0) {
-		display_filter(resultdiv, sorted_meta, filter);
-		return;
+                display_filter(resultdiv, sorted_meta, filter);
+                return;
 	    }
 	    // hide all
 	    for (const item in sorted_meta) {
-		const id = (sorted_meta[item].brand + "-" + sorted_meta[item].model).replace(/['.+& ]/g, "-");
-		$("#" + id).hide();
+                const id = (sorted_meta[item].brand + "-" + sorted_meta[item].model).replace(/['.+& ]/g, "-");
+                $("#" + id).hide();
 	    }
 	    // minScore
 	    let minScore = 1;
 	    for (const item in results) {
-		if (results[item].score < minScore) {
+                if (results[item].score < minScore) {
 		    minScore = results[item].score;
-		}
+                }
 	    }
 	    // console.log("minScore is "+minScore);
 	    for (let item in results) {
-		let show = true;
-		let result = results[item];
-		let item_meta = result.item;
-		let score = result.score;
-		// console.log("evaluating "+item_meta.brand+" "+item_meta.model+" "+score);
-		if (!is_filtered(item_meta, filter)) {
+                let show = true;
+                let result = results[item];
+                let item_meta = result.item;
+                let score = result.score;
+                // console.log("evaluating "+item_meta.brand+" "+item_meta.model+" "+score);
+                if (!is_filtered(item_meta, filter)) {
 		    // console.log("filtered out (filter)");
 		    show = false;
-		}
-		if (show) {
+                }
+                if (show) {
 		    if (minScore < Math.pow(10, -15)) {
                         // we have an exact match, only show other exact matches
                         if (score >= Math.pow(10, -15)) {
@@ -193,20 +193,20 @@ $(document).ready(function () {
                         if (score > minScore*10) {
 		            // console.log("filtered out (score="+score+"minscore="+minScore+")");
 			    show = false;
-			}
+                        }
 		    }
-		}
-		const id = (item_meta.brand + "-" + item_meta.model).replace(/['.+& ]/g, "-");
-		if (show) {
+                }
+                const id = (item_meta.brand + "-" + item_meta.model).replace(/['.+& ]/g, "-");
+                if (show) {
 		    // console.log("show "+item_meta.brand+" "+item_meta.model+" "+score);
 		    $("#" + id).show();
-		} else {
+                } else {
 		    // console.log("hide "+item_meta.brand+" "+item_meta.model+" "+score);
 		    $("#" + id).hide();
-		}
+                }
 
 	    }
-	}
+        }
 
         function get_price(item) {
             if (item.id in indirect) {
@@ -260,7 +260,7 @@ $(document).ready(function () {
             return 19700101;
         }
 
-	function sort_metadata(current_sorter) {
+        function sort_metadata(current_sorter) {
             // console.log("starting sort + sort_by:"+current_sorter.by);
             // TODO build once
             let sorted = {};
@@ -288,10 +288,10 @@ $(document).ready(function () {
             if ( sorted.length > 1 ) {
 	        $("div.searchresults > div").html(sorted);
             }
-	}
+        }
 
     }).fail(function (jqXHR, textStatus) {
-	// console.log("getJSON request failed! " + textStatus);
+        // console.log("getJSON request failed! " + textStatus);
     });
 
 });
