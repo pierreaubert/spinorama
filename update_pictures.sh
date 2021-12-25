d3775045a7679ab3d44c7eef63e81fec6c57906d
pierreaubert <pierre0aubert@gmail.com>
2021-12-25 13:08:41 +0100

    generates .webp and .avif to decrease size of the network

--- update_pictures.sh
+++ update_pictures.sh
@@ -8,16 +8,21 @@ mkdir -p ${targetdir}/pictures
 for d in "${sourcedir}/pictures"; do
     find $d -type f -name '*.png'| while read pict; do
 	smaller=$targetdir${pict#$sourcedir}
-	smaller=${smaller%.png}.jpg
-	if ! test -f "$smaller"; then
-	    convert "$pict" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "$smaller";
-	fi
+        for t in "jpg" "webp" "avif"; do
+	    smallert=${smaller%.png}.${t}
+	    if ! test -f "$smallert"; then
+	        echo convert "$pict" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "$smallert";
+	    fi
+        done
     done
     find $d -type f -name '*.jpg'| while read pict; do
-	smaller=$targetdir${pict#$sourcedir}
-	if ! test -f "$smaller"; then
-	    convert "$pict" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "$smaller";
-	fi
+        smaller=$targetdir${pict#$sourcedir}
+        for t in "webp" "avif"; do
+	    smallert=${smaller%.jpg}.${t}
+	    if ! test -f "$smaller"; then
+	        convert "$pict" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "$smallert";
+	    fi
+        done
     done
 done
 # copy logs
@@ -25,8 +30,14 @@ cp datas/logos/* docs/logos
 
 # reduce size of large image
 find docs  -type f -name '*_large.png' -print | while read pict; do
-    target="${pict%_large.png}.jpg"
-    if ! test -f "${target}"; then
-	convert "$pict" -quality 75 "${target}"
-    fi
+    for t in "jpg" "webp" "avif"; do
+        target="${pict%_large.png}.${t}"
+        if ! test -f "${target}"; then
+            if [ "$t" = "jpg" ]; then
+	        convert "$pict" -quality 75 "${target}"
+            else
+	        convert "$pict" "${target}"
+            fi
+        fi
+    done
 done
