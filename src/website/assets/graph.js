aa3bf464f15639342ea36fc84239eed5144255d3
pierreaubert <pierre0aubert@gmail.com>
2021-12-28 09:29:52 +0100

    fix: graphs on small screen

--- src/website/assets/graph.js
+++ src/website/assets/graph.js
@@ -2,23 +2,68 @@ function displayGraph(spec, divName) {
 
     async function run() {
         w = window.innerWidth;
-        h = window.innerHeight-200;
-        if (w/1.414 > h) {
-            w =	h*1.414;
+        h = window.innerHeight;
+        console.log('IN w='+w+' h='+h);
+        if (w < h) {
+            h =	Math.min(w/1.414+200, h);
         } else {
-            h /= 1.414;
+            w = h/1.414;
+        }
+        console.log('OUT w='+w+' h='+h);
+        spec.layout.width = w;
+        spec.layout.height = h;
+        if (w > 640) {
+	    spec.layout.margin = {
+	        'l': 15,
+	        'r': 15,
+	        't': 50,
+	        'b': 50,
+	    };
+            spec.layout.legend = {
+                'orientation': 'h',
+                'y': -0.2,
+                'x': 0,
+                'xanchor': 'bottom',
+                'yanchor': 'left',
+            };
+        } else {
+            spec.layout.yaxis.visible = false;
+            spec.layout.yaxis2.visible = false;
+	    spec.layout.margin = {
+	        'l': 0,
+	        'r': 0,
+	        't': 100,
+	        'b': 100,
+	    };
+            spec.layout.legend = {
+                'orientation': 'h',
+                'y': -0.4,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'left',
+            };
+            var title = spec.layout.title.text;
+            var pos = title.indexOf('measured');
+            spec.layout.title = {
+                'text': title.slice(0, pos),
+                'orientation': 'h',
+                'y': 0.85,
+                'x': 0.5,
+                'xanchor': 'center',
+                'yanchor': 'left',
+            };
         }
         Plotly.newPlot(divName, spec.data, spec.layout);
     }
     run();
 }
 
-function displayStats(spec, divName) {
-
-    async function run() {
-        spec.layout.width = 1000;
-        spec.layout.height = 800;
-        Plotly.newPlot(divName, spec.data, spec.layout);
-    }
-    run();
-}
+//function displayStats(spec, divName) {
+//
+//    async function run() {
+//        spec.layout.width = window.innerWidth;
+//        spec.layout.height = window.innerHeight;
+//        Plotly.newPlot(divName, spec.data, spec.layout);
+//    }
+//    run();
+//}
