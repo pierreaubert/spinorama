4f9a4d2f20fa8e243a58f640f59c21ac2aeb751e
pierreaubert <pierre0aubert@gmail.com>
2021-12-31 09:12:43 +0100

    fix: add missing jpg pictures of speakers

--- update_pictures.sh
+++ update_pictures.sh
@@ -8,7 +8,7 @@ mkdir -p ${targetdir}/pictures
 for d in "${sourcedir}/pictures"; do
     find $d -type f -name '*.png'| while read pict; do
 	smaller=$targetdir${pict#$sourcedir}
-        for t in "jpg" "webp" "avif"; do
+        for t in "jpg" "webp"; do
 	    smallert=${smaller%.png}.${t}
 	    if ! test -f "$smallert"; then
 	        convert "$pict" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "$smallert";
@@ -17,7 +17,7 @@ for d in "${sourcedir}/pictures"; do
     done
     find $d -type f -name '*.jpg'| while read pict; do
         smaller=$targetdir${pict#$sourcedir}
-        for t in "webp" "avif"; do
+        for t in "jpg" "webp"; do
 	    smallert=${smaller%.jpg}.${t}
 	    if ! test -f "$smaller"; then
 	        convert "$pict" -define jpeg:size=300x500  -thumbnail '200x300>' -gravity center -extent 200x300 "$smallert";
