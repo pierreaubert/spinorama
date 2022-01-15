d00a641c632a857ba6dcc8603743d4b096dc9ea9
pierreaubert <pierre0aubert@gmail.com>
2022-01-15 20:56:12 +0100

    fix 404 checking

--- check_404.sh
+++ check_404.sh
@@ -1,2 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
 pylinkvalidate.py -P http://spinorama.internet-box.ch
+
+for f in docs/speakers/*/*/*/*.html; do
+    name=${f#docs/}
+    u=${name// /%20}
+    pylinkvalidate.py -P "http://spinorama.internet-box.ch/$u"
+done
