4819e67499b698be8d64fce5be101237d64f515e
pierreaubert <pierre0aubert@gmail.com>
2022-01-17 09:01:30 +0100

    add configuration file for vs-code

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((js-mode . ((flycheck-checker . javascript-standar))))
+((js-mode . ((flycheck-checker . javascript-standard))))
