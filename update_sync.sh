2ec187f3f751f59e7ef1a06c5b848bcf2c7d8779
pierreaubert <pierre0aubert@gmail.com>
2022-03-01 09:06:51 +0100

    add a test for dev url in prod site

--- update_sync.sh
+++ update_sync.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 TARGET=$HOME/src/pierreaubert.github.io/spinorama
+# check
+command=$(grep internet-box docs/*.html | wc -l)
+if [ $command -ne 0 ]; then
+    echo "KO found dev url in prod site"
+    exit 1;
+else
+    echo "OK checking for dev site in prod"
+fi
 # copy
 echo "Sync"
 rsync -arv --exclude '*.png' --delete ./docs/* $TARGET
