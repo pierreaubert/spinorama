e54ef3b6049416b4fd3b56b04b7405e671c39d3e
pierreaubert <pierre0aubert@gmail.com>
2022-09-27 21:30:48 +0200

    fix: small issues

--- update_reviewers.sh
+++ update_reviewers.sh
@@ -4,6 +4,6 @@ json_pp < docs/assets/metadata.json  | \
     grep -e '"misc-' | \
     grep -v default | \
     sed -e s'/[ \t"":{"]//g' | \
-    sed -e 's/misc-//' -e 's/-horizontal//g' -e 's/-vertical//g' | \
+    sed -e 's/misc-//' -e 's/-horizontal//g' -e 's/-vertical//g' -e 's/-sealed//g' -e 's/-ported//g' | \
     sort -s -f -u | \
     awk '{val=toupper(substr($0,1,1)); name=substr($0,2); if (length($0) < 4 ) { val=""; name=toupper($0); } printf("<option value=\"%s\">%s%s</option>\n", $0, val, name);}' > src/website/reviewers.html
