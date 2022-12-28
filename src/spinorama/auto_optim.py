1b302728f07b8654ed7c50430ddeffdabbc0b23d
pierreaubert <pierre0aubert@gmail.com>
2022-12-28 13:55:47 +0100

    updated some deprecated functions in numpy

--- src/spinorama/auto_optim.py
+++ src/spinorama/auto_optim.py
@@ -136,8 +136,8 @@ def savitzky_golay(y, window_size, order, deriv=0, rate=1):
        Cambridge University Press ISBN-13: 9780521880688
     """
     try:
-        window_size = np.abs(np.int(window_size))
-        order = np.abs(np.int(order))
+        window_size = abs(int(window_size))
+        order = abs(int(order))
     except ValueError as msg:
         raise ValueError("window_size and order have to be of type int")
     if window_size % 2 != 1 or window_size < 1:
