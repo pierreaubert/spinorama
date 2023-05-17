8538fd0438966e3be130dfd04b3f1a42d81cdc73
pierreaubert <pierre0aubert@gmail.com>
2023-05-17 12:46:49 +0200

    fix: add new parameter in test case

--- tests/test_scores.py
+++ tests/test_scores.py
@@ -47,7 +47,7 @@ class PrefRatingTests(unittest.TestCase):
         db = [100 for i in np.logspace(0.3, 4.3, 1000)]
         test_df = pd.DataFrame({"Freq": freq, "dB": db})
         # expect 0 deviation from flat line
-        self.assertEqual(aad(test_df), 0.0)
+        self.assertEqual(aad(test_df, 20), 0.0)
 
 
 if __name__ == "__main__":
