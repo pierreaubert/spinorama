8970b1ae8c4b33bf272bd164dc28dff105f213c0
pierreaubert <1709343+pierreaubert@users.noreply.github.com>
2024-01-04 11:23:33 +0100

    update copyright to 2024

---
+++ scripts/update_etags.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+etags \
+      *.py *.sh *.txt *.md \
+      src/*/*.py src/*/*.html src/*/*/*.js \
+      tests/*.py tests/*/*.md tests/*/*.[ch]pp tests/*/*.py tests/*/*.sh \
+      scripts/*.py scripts/*.sh scripts/*.awk \
+      datas/*.py \
+      tutorial/*.md tutorial/*/*.md 
+      
+      
+      
+      
+      
