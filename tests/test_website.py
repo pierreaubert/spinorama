42abe5e3a2ded1c6b3de240d7f70edc684ea4b4a
pierreaubert <1709343+pierreaubert@users.noreply.github.com>
2024-01-30 11:38:45 +0100

    fix: add another option to prevent crashed in selenium

--- tests/test_website.py
+++ tests/test_website.py
@@ -17,6 +17,8 @@ class SpinoramaWebsiteTests(unittest.TestCase):
     def setUp(self):
         options = webdriver.ChromeOptions()
         options.add_argument("--headless=new")
+        # unclear why we need that option but selenium crashes without it
+        options.add_argument('--remote-debugging-pipe')
         self.driver = webdriver.Chrome(options=options)
 
     def tearDown(self):
