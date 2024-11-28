1a1bd3938ad4c8db12d973d8e202b4c6c2261412
pierre aubert <pierre@spinorama.org>
2024-11-28 14:59:50 +0100

    updated plotly to last version

--- scripts/update_dev.sh
+++ scripts/update_dev.sh
@@ -22,7 +22,7 @@
 # TARGET=pierre@ch.spinorama.org:/var/www/html/spinorama-dev
 # TARGET=pierre@es.spinorama.org:/var/www/html/spinorama-dev
 # TARGET=pierre@web:/var/www/html/spinorama-dev
-TARGET=pierre@192.168.1.19:/var/www/html/spinorama-dev
+TARGET=pierre@192.168.1.18:/var/www/html/spinorama-dev
 # check
 command=$(grep www.spinorama.org docs/*.html | wc -l)
 if [ $command -ne 0 ]; then
