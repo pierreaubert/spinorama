f807ab03ae60b8ee8bf4819bf3919bd5a809619c
pierre aubert <pierre@spinorama.org>
2025-01-23 06:49:40 +0100

    added some incomplete measurements

--- datas/incomplete.py
+++ datas/incomplete.py
@@ -6,6 +6,11 @@ The set is used to decrease verbosity of warnings.
 known_incomplete_measurements = set(
     [
         ("Andersson HIS 2.1", "misc-ageve"),
+        ("Buchardt Audio S300", "misc-speakerdata2034"),  # missing ER
+        ("Buchardt Audio S400", "vendor"),  # missing ER
+        ("JBL LSR308", "misc-speakerdata2034"),  # missing LW
+        ("KEF R11", "misc-speakerdata2034"),  # only ON and LW
         ("Paradigm Millenia LP 2", "misc-matthews"),
+        ("Polk Audio Legend L800", "audioholics"),  # missing SP
     ]
 )
