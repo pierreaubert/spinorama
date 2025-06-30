c48f323677635775d0966a5dc4bfb7e6fb6dd886
pierre aubert <pierre@spinorama.org>
2025-06-30 13:14:11 +0200

    fix: 404

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,14 @@ import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
+    /*
+        inspectBrk: true,
+        fileParallelism: false,
+	browser: {
+	    provider: 'playwright',
+	    instances: [{ browser: 'chromium' }]
+	},
+    */
         include: ['**/src/website/*.test.js', '**/tests/*.test.js'],
         exclude: [...configDefaults.exclude, '**/dist/**', '**/build/**'],
         watch: false,
